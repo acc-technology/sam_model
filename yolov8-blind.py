14bc02e4d7e44b0a3fc154b90da2660545280589
Isaray <10184500631@stu.ecnu.edu.cn>
2024-02-15 00:11:11 +0800

    feat: train models with total data in train3

--- yolov8-blind.py
+++ yolov8-blind.py
@@ -6,8 +6,9 @@ from io import BytesIO
 import cv2
 
 # model = YOLO("yolov8n.pt")
-model = YOLO('runs/detect/train/weights/best.pt')
-image = cv2.imread('pic/OIP-C.jpg')
+model = YOLO('runs/detect/train3/weights/best.pt')
+pic_name="blind_2"
+image = cv2.imread('pic/{}.jpg'.format(pic_name))
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 image = cv2.resize(image, (int(image.shape[1]/2.5), int(image.shape[0]/2.5)))
 results = model.predict(image)
@@ -31,7 +32,7 @@ def box_label(image, box, label='', color=(128, 128, 128), txt_color=(255, 255, 
 def plot_bboxes(image, boxes, labels=[], colors=[], score=True, conf=None):
   #Define COCO Labels
   if labels == []:
-    labels={0:'walk',1:'T',2:'L',3:'bike',4:'car',5:'cover',6:'column'}
+    labels={1:'walk',2:'T',3:'L',4:'car',5:'bike',6:'cover',7:'column'}
     # labels = {0: u'__background__', 1: u'food', 2: u'bicycle',3: u'car', 4: u'motorcycle', 5: u'airplane', 6: u'bus', 7: u'train', 8: u'truck', 9: u'boat', 10: u'traffic light', 11: u'fire hydrant', 12: u'stop sign', 13: u'parking meter', 14: u'bench', 15: u'bird', 16: u'cat', 17: u'dog', 18: u'horse', 19: u'sheep', 20: u'cow', 21: u'elephant', 22: u'bear', 23: u'zebra', 24: u'giraffe', 25: u'backpack', 26: u'umbrella', 27: u'handbag', 28: u'tie', 29: u'suitcase', 30: u'frisbee', 31: u'skis', 32: u'snowboard', 33: u'sports ball', 34: u'kite', 35: u'baseball bat', 36: u'baseball glove', 37: u'skateboard', 38: u'surfboard', 39: u'tennis racket', 40: u'bottle', 41: u'wine glass', 42: u'cup', 43: u'fork', 44: u'knife', 45: u'spoon', 46: u'bowl', 47: u'banana', 48: u'apple', 49: u'sandwich', 50: u'orange', 51: u'broccoli', 52: u'carrot', 53: u'hot dog', 54: u'pizza', 55: u'donut', 56: u'cake', 57: u'chair', 58: u'couch', 59: u'potted plant', 60: u'bed', 61: u'dining table', 62: u'toilet', 63: u'tv', 64: u'laptop', 65: u'mouse', 66: u'remote', 67: u'keyboard', 68: u'cell phone', 69: u'microwave', 70: u'oven', 71: u'toaster', 72: u'sink', 73: u'refrigerator', 74: u'book', 75: u'clock', 76: u'vase', 77: u'scissors', 78: u'teddy bear', 79: u'hair drier', 80: u'toothbrush'}
     # labels = {0: u'__background__', 1: u'person', 2: u'bicycle',3: u'car', 4: u'motorcycle', 5: u'airplane', 6: u'bus', 7: u'train', 8: u'truck', 9: u'boat', 10: u'traffic light', 11: u'fire hydrant', 12: u'stop sign', 13: u'parking meter', 14: u'bench', 15: u'bird', 16: u'cat', 17: u'dog', 18: u'horse', 19: u'sheep', 20: u'cow', 21: u'elephant', 22: u'bear', 23: u'zebra', 24: u'giraffe', 25: u'backpack', 26: u'umbrella', 27: u'handbag', 28: u'tie', 29: u'suitcase', 30: u'frisbee', 31: u'skis', 32: u'snowboard', 33: u'sports ball', 34: u'kite', 35: u'baseball bat', 36: u'baseball glove', 37: u'skateboard', 38: u'surfboard', 39: u'tennis racket', 40: u'bottle', 41: u'wine glass', 42: u'cup', 43: u'fork', 44: u'knife', 45: u'spoon', 46: u'bowl', 47: u'banana', 48: u'apple', 49: u'sandwich', 50: u'orange', 51: u'broccoli', 52: u'carrot', 53: u'hot dog', 54: u'pizza', 55: u'donut', 56: u'cake', 57: u'chair', 58: u'couch', 59: u'potted plant', 60: u'bed', 61: u'dining table', 62: u'toilet', 63: u'tv', 64: u'laptop', 65: u'mouse', 66: u'remote', 67: u'keyboard', 68: u'cell phone', 69: u'microwave', 70: u'oven', 71: u'toaster', 72: u'sink', 73: u'refrigerator', 74: u'book', 75: u'clock', 76: u'vase', 77: u'scissors', 78: u'teddy bear', 79: u'hair drier', 80: u'toothbrush'}
   #Define colors
@@ -57,6 +58,6 @@ def plot_bboxes(image, boxes, labels=[], colors=[], score=True, conf=None):
   #show image
   image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 #   cv2.imshow(image) #if used in Python
-  cv2.imwrite('pic/yolo_res_blind.jpg',image)
+  cv2.imwrite('pic/yolo_res_{}.jpg'.format(pic_name),image)
 plot_bboxes(image, results[0].boxes.data, score=False)
 
